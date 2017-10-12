69817f2badaa60716d6d2c161293e7c037859159
Ryan Walker <ryanwalker7@gmail.com>
2017-10-12 14:14:25 -0700

    Attmepting to see available bluetooth outputs

--- HandsfreeBluetoothRouter/ViewController.swift
+++ HandsfreeBluetoothRouter/ViewController.swift
@@ -7,11 +7,29 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let audioSession = AVAudioSession()
+        
+        do {
+             try audioSession.setCategory(AVAudioSessionCategoryMultiRoute)
+            try audioSession.setActive(true)
+            print(audioSession.currentRoute.outputs)
+            print(audioSession.outputDataSources)
+        } catch is Error {
+            print("error")
+        }
+        
+        //https://stackoverflow.com/questions/15643516/list-available-output-audio-target-avaudiosession
+        print(audioSession.currentRoute)
+//        audioSession.setCategory(AVAudioSessionCategorySoloAmbient, AVAudioSessionCategoryOptions.allowBluetooth)
+        print(audioSession.availableCategories)
+        print(audioSession.categoryOptions)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
