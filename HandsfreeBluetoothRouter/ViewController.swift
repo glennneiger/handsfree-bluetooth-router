a6a8cef42ee7e59498115e934d879fd191b418c4
Ryan Walker <ryan.walker@infusionsoft.com>
2018-11-12 11:10:29 -0700

    AVAudioSession just about set up

--- HandsfreeBluetoothRouter/ViewController.swift
+++ HandsfreeBluetoothRouter/ViewController.swift
@@ -7,18 +7,38 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var audioSession: AVAudioSession!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        audioSession = AVAudioSession.sharedInstance()
+        
+        do {
+            try audioSession.setCategory(AVAudioSessionCategoryPlayback, with: [AVAudioSessionCategoryOptions.allowBluetooth])
+            
+            AVAudioSessionCategoryOptions.allowBluetooth
+            
+//        audioSession.setMode()
+        } catch {
+            
+        }
+        //this should happen when clicking a button
+//        audioSession.setActive(true)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
 
 }
