0c4701765e604dc4d9ae28c3de29ba8816a4d0a8
Ryan Walker <ryan.walker@infusionsoft.com>
2018-11-13 09:04:30 -0700

    Added AVRoutePicker

--- HandsfreeBluetoothRouter/ViewController.swift
+++ HandsfreeBluetoothRouter/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import AVFoundation
+import AVKit
 
 class ViewController: UIViewController {
 
@@ -16,20 +17,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let myRoutePickerView = AVRoutePickerView()
+        myRoutePickerView.frame = CGRect(x: 20, y: 20, width: 50, height: 50)
+        myRoutePickerView.delegate = self
+        self.view.addSubview(myRoutePickerView)
         
         audioSession = AVAudioSession.sharedInstance()
         
         do {
-            try audioSession.setCategory(AVAudioSessionCategoryPlayback, with: [AVAudioSessionCategoryOptions.allowBluetooth])
-            
-            AVAudioSessionCategoryOptions.allowBluetooth
-            
-//        audioSession.setMode()
+//            try audioSession.setCategory(AVAudioSessionCategoryPlayAndRecord, with: [AVAudioSessionCategoryOptions.allowBluetooth])
+            try audioSession.setCategory(AVAudioSessionCategoryPlayAndRecord, mode: AVAudioSessionModeDefault, routeSharingPolicy: .longForm, options: [AVAudioSessionCategoryOptions.allowBluetooth])
         } catch {
             
         }
-        //this should happen when clicking a button
-//        audioSession.setActive(true)
         
     }
 
@@ -39,7 +39,35 @@ class ViewController: UIViewController {
     }
     
     
-
+    @IBAction func play(_ sender: Any) {
+        do {
+        try audioSession.setActive(true)
+        } catch {
+            
+        }
+        var text = ""
+        let inputs = audioSession.availableInputs
+        inputs?.forEach({ portDesc in
+            if portDesc.portType == AVAudioSessionPortBluetoothHFP {
+                do {
+                    try audioSession.setPreferredInput(portDesc)
+                } catch {}
+            }
+            text.append("Port Type: \(portDesc.portType)\n")
+        })
+        textArea.text = text
+        
+        
+        
+        
+        
+    }
+    
+    @IBOutlet weak var textArea: UITextView!
+    
 
 }
 
+extension ViewController: AVRoutePickerViewDelegate {
+    
+}
